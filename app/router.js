e0a956a81f29f3a52317e6cad7d6a9c7020b0429
hoangvv2107 <hoangvv.2107@gmail.com>
2026-07-06 00:42:57 +0700

    feat(router): enhance routing by adding product and report navigation

--- app/router.js
+++ app/router.js
@@ -1,14 +1,23 @@
 import Navigo from "navigo";
 import { mount as mountLogin } from "./login/main.js";
 import { mount as mountCustomers } from "./customers/main.js";
+import {
+  mountProductListPage,
+  mountProductFormPage,
+} from "./products/product.js";
 
 export const router = new Navigo("/", { hash: true });
 
 const app = document.getElementById("app");
 
 router
-    .on("/login", () => mountLogin(app))
-    .on("/customers", () => mountCustomers(app))
-    .notFound(() => router.navigate("/login"));
+  .on("/home", () => router.navigate("/products"))
+  .on("/login", () => mountLogin(app))
+  .on("/customers", () => mountCustomers(app))
+  .on("/products", () => mountProductListPage(app))
+  .on("/products/create", () => mountProductFormPage(app))
+  .on("/orders", () => router.navigate("/products"))
+  .on("/reports", () => router.navigate("/products"))
+  .notFound(() => router.navigate("/products"));
 
 router.resolve();
